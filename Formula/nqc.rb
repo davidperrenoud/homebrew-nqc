dc578432208328663c0da51849538a079dc8ced8
David Perrenoud <639875+davidperrenoud@users.noreply.github.com>
2022-10-03 22:10:15 +0000

    nqc: add 3.2r1 bottle.

--- Formula/nqc.rb
+++ Formula/nqc.rb
@@ -5,6 +5,11 @@ class Nqc < Formula
   version "3.2r1"
   sha256 "057ca37e92d4eb7b0418e4c503ed9df2e1d057dc30e9426ce3072ee6eb75271a"
   license "MPL-2.0"
+
+  bottle do
+    root_url "https://github.com/davidperrenoud/homebrew-nqc/releases/download/nqc-3.2r1"
+    sha256 cellar: :any_skip_relocation, monterey: "d705c6381ec6bd746b9383f7a6a8335d0029a1c0c6ebe51d3aaf9a7d4cbb9ee4"
+  end
   depends_on :macos
 
   def install
