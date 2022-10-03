a3535b128d2aec52fd8b3455f4b7dc3b02de11a1
David Perrenoud <davidperrenoud@users.noreply.github.com>
2022-10-03 23:49:08 +0200

    Update nqc.rb

--- Formula/nqc.rb
+++ Formula/nqc.rb
@@ -5,6 +5,7 @@ class Nqc < Formula
   version "3.2r1"
   sha256 "057ca37e92d4eb7b0418e4c503ed9df2e1d057dc30e9426ce3072ee6eb75271a"
   license "MPL-2.0"
+  depends_on :macos
 
   def install
     ENV.deparallelize
